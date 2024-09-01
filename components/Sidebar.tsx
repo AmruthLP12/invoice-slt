5ae32151911b92598f6a792e567d528672232959
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-09-01 20:09:21 +0530

    href updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -43,7 +43,7 @@ export function Sidebar() {
     },
     {
       label: "DashBoard",
-      href: "#", // Updated path
+      href: "/dashboard", // Updated path
       icon: (
         <IconLayoutDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
